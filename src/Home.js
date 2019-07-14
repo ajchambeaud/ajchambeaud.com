2be50fff3c11d8b7a4cebaabe724e7ccce10fac7
ajchambeaud <ajchambeaud@gmail.com>
2019-07-14 00:03:21 -0300

    bootstraped CRA

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className="App-link">ajchambeaud.com - soon (maybe not).</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
